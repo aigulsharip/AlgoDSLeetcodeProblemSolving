5392ea1fab9d49c77f9b044ba5c6ae703456157a
aigulsharip <aigul.sharip@nu.edu.kz>
2024-04-17 06:51:11 +0500

    Added MoveZeroes

---
+++ src/test/java/com/example/algods_leetcodeproblemsolving/MoveZeroes.java
@@ -0,0 +1,18 @@
+package com.example.algods_leetcodeproblemsolving;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int j = 0;
+        for (int i = 0; i < n; i++ ) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+
+        }
+        while (j < n) {
+            nums[j++] = 0;
+        }
+
+    }
+}
--- src/test/java/com/example/algods_leetcodeproblemsolving/SingleNumber.java
+++ src/test/java/com/example/algods_leetcodeproblemsolving/SingleNumber.java
@@ -1,2 +1,12 @@
-package com.example.algods_leetcodeproblemsolving;public class SingleNumber {
+package com.example.algods_leetcodeproblemsolving;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+
+        for (int i: nums) {
+            singleNumber = i ^singleNumber;
+        }
+        return singleNumber;
+    }
 }
