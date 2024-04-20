ea24be7a96e04c534b1a0eb1c036ea210573c88c
aigulsharip <aigul.sharip@nu.edu.kz>
2024-04-20 08:59:49 +0500

    Added Valid Sudoku solutions

---
+++ src/test/java/com/example/algods_leetcodeproblemsolving/ValidSudoku.java
@@ -0,0 +1,23 @@
+package com.example.algods_leetcodeproblemsolving;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    if (!seen.add(num  + "in row " + i) ||
+                            !seen.add(num  + "in col " + j) ||
+                            !seen.add(num  + "in subbox " + i/3 + "-" + j/3)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
